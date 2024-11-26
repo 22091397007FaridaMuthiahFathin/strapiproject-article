4c2262de3740fd9d05c6aefff7ba2ecb9b1779f6
farida <faridfathin05@gmail.com>
2024-11-26 09:36:37 +0700

    first commit

---
+++ src/api/user/customUser.ts
@@ -0,0 +1,19 @@
+// src/api/user/customUser .ts
+import { Context } from 'koa';
+
+export default {
+  async me(ctx: Context) {
+    const { user } = ctx.state;
+    if (!user) {
+      return ctx.unauthorized('You must be logged in to access this.');
+    }
+
+    // Mengambil pengguna dengan role
+    const entity = await strapi.entityService.findOne('plugin::users-permissions.user', user.id, {
+      populate: ['role'],
+    });
+
+    // Mengembalikan data yang telah dibersihkan
+    return entity;
+  },
+};
